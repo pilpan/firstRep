2cca0a216f5fa5acbab74522a60a95003bfe377c
plip <plip2010195@yandex.ru>
2024-04-02 18:02:57 +0300

    init Git repo

---
+++ script.js
@@ -0,0 +1,58 @@
+// Определяем переменные с помощью разных ключевых слов
+var myVar = 5;
+let myLet = 10;
+const myConst = 15;
+
+// Типы данных
+let number = -20;
+let string = 'Привет, мир!';
+let boolean = true;
+let object = {
+    key: 'value', obj: {
+        a: '2'
+    }
+};
+let array = [1, 2, 3, 4, 5];
+// Операторы
+let sum = myVar + myLet; // % /
+let isEqual = myVar === myLet; // == === < >
+
+// Условные операторы
+if (number > 10) {
+    console.log('Число больше 10');
+} else {
+    console.log('Число меньше или равно 10');
+}
+
+// Циклы
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+let test = ''
+// Функции
+function greet(name = 'Алексей') {
+    test = name[0]
+    // name = name.toUpperCase()
+    return 'Привет, '+name;
+}
+
+let plus = (a,b) => {
+    return a+b
+}
+let greeting = greet('Миша');
+
+// Работа с DOM
+const button = document.getElementById('myButton');
+const output = document.getElementById('output');
+let emptyPage = document.querySelector('.emptyPage')
+console.log(emptyPage);
+emptyPage.innerText = '23'
+emptyPage.innerHTML = `<a href='#'>33</a>`
+emptyPage.remove()
+
+button.addEventListener('click', function () {
+    output.innerHTML = greeting;
+});
+
+console.log(greet('123').a);
+console.log(plus(4,5));
